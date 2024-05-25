a79d8a8de605b597c9d41c04c2cc31353b6ab4f3
moeburn <moeburn@gmail.com>
2024-05-25 07:57:01 -0400

    recalibrate temp

--- temp_display.ino
+++ temp_display.ino
@@ -2,29 +2,23 @@
 
 SH1106Wire display(0x3c, SDA, SCL, GEOMETRY_128_64, I2C_ONE, 400000); 
 
-#include <OneWire.h>  //All Dallas code has been temporarily removed, to be replaced by Dallas EEPROM calibrator
-#include <DallasTemperature.h>
-#include <NonBlockingDallas.h>
+
 #include <FastLED.h>
 #include <SteinhartHart.h>
-#include <Adafruit_ADS1X15.h>
-
-Adafruit_ADS1115 ads;
 
 
-SteinhartHart thermistor(2930,7248,14800, 328.59, 305.71, 289.65);
+//SteinhartHart thermistor(2930,7248,14800, 328.59, 305.71, 289.65);
+SteinhartHart thermistor(905,1900,3133, 338.65, 310.75, 288.55);
 int16_t adc0;
 
 #define LED_PIN 1  
-#define ONE_WIRE_BUS 2
+#define ADC_PIN 0
 #define TIME_INTERVAL 1500    
 
 #define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature dallasTemp(&oneWire);
-NonBlockingDallas sensorDs18b20(&dallasTemp);
+
 
 #define every(interval) \        
   static uint32_t __every__##interval = millis(); \
@@ -36,12 +30,7 @@ float tempC, tempTherm;
 	uint8_t blue = 0;
 	double hue, saturation, lighting, value;
 
-void handleTemperatureChange(int deviceIndex, int32_t temperatureRAW)
-{
 
-  tempC = sensorDs18b20.rawToCelsius(temperatureRAW);
-
-}
 
 long mapf(float x, float in_min, float in_max, float out_min, float out_max)
 {
@@ -50,61 +39,43 @@ long mapf(float x, float in_min, float in_max, float out_min, float out_max)
 
 
 void setup() {
-  //Serial.begin(9600);
-  ads.setGain(GAIN_ONE); 
-  ads.begin();
+  Serial.begin(9600);
+
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(255);
-  sensorDs18b20.begin(NonBlockingDallas::resolution_12, TIME_INTERVAL); //use non-blocking DS18b20 library
-  sensorDs18b20.onTemperatureChange(handleTemperatureChange);  //only notice when DS18B20 changes
   display.init();
   display.setFont(SansSerif_plain_30);
- // display.setFont(ArialMT_Plain_16);
-
-  //display.flipScreenVertically();
-  display.setBrightness(255); 
+  display.flipScreenVertically();
+  //display.setFont(ArialMT_Plain_16);
+  display.setBrightness(128); 
 
 }
 
 void loop() {
   delay(10);
-  //sensorDs18b20.update();
-  display.clear();
-  //String tempstring = String(tempC) + "°C"; 
 
-  /*long adsread;
-  for (int i = 0; i < 50; i++) {
-      adsread += ads.readADC_SingleEnded(0);
-  }*/
-  adc0 = ads.readADC_SingleEnded(0);
-  //String thermstring = String(tempTherm) + "°C"; 
-  float V = ads.computeVolts(adc0);
-  float  R2 = (-V * 10000) / (V - 3.3);    
-  tempTherm = thermistor.resistanceToTemperature(R2) - 273.15;
+  display.clear();
+
+  adc0 = analogRead(ADC_PIN);
+
+  tempTherm = thermistor.resistanceToTemperature(adc0) - 273.15;
+  //ADC0: 2800, V: 2.26, R2: 21621.63, Temp: -94.89
   blue = mapf(tempTherm, 15, 40, 255, 0);
   red = mapf(tempTherm, 15, 40, 0, 255);
   green = mapf(tempTherm, 40, 60, 0, 255);
+  if (tempTherm < 15) {green = 0; red = 0; blue = 255;}
   if (tempTherm < 40) {green = 0;}
   if (tempTherm > 40) {red = 255; blue = 0;}
   if (tempTherm > 60) {green = 255;}
-  String thermstring = String(tempTherm, 2)+ "°C"; 
-  //String rgbstring = String(red) + "," + String(green) + "," + String(blue);
-  //display.drawString(0, 0, tempstring);
-    //display.setFont(SansSerif_plain_30);
-
+  every(100){
+  String thermstring = String(tempTherm, 1)+ "°C"; 
+  //String adcstring = String(adc0); 
+  //display.setFont(SansSerif_plain_30);
   display.drawString(0, 16, thermstring);
-   // display.setFont(ArialMT_Plain_16);
-  //display.drawString(0, 32, rgbstring);
+  //display.setFont(ArialMT_Plain_16);
+  //display.drawString(0, 48, adcstring);
   display.display();
-  //Serial.println(tempTherm);
-
-
-  /*if (tempTherm < 40) {hue = map(tempTherm, 10,40,243,360);}
-  if (tempTherm >= 40) {hue = map(tempTherm, 40,60,0,60);}
-  hue = map(hue, 0, 360, 0, 255);*/
-
+  }
   leds[0] = CRGB(red, green, blue);
   FastLED.show();
-
-  
 }
